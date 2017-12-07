0fe8ba5b79418ee343c27d10f321da969727360d
Pasupatirajesh <pasupatirajesh@gmail.com>
2017-12-07 13:38:54 -0600

    Build Flavor Error

--- app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -12,8 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.android.androidlib.LibraryActivity;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 
@@ -40,14 +38,6 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
 
         mJokeButton = (Button) root.findViewById(R.id.button2);
 
